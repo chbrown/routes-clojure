0b4699812a5ce88aed39ebaa552f9a25978a904d
Christopher Brown <io@henrian.com>
2017-08-24 11:32:43 -0500

    Move clojure 1.8.0 dependency to root project config for clojure.string/index-of, etc.

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {:builds [{:source-paths ["src"]
                         :compiler {:optimizations :simple
                                    :pretty-print false}}]}
   :profiles {:test {:plugins [[lein-cloverage "1.0.9"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/clojurescript "1.9.908"]
+             :dev {:dependencies [[org.clojure/clojurescript "1.9.908"]
                                   [org.clojure/tools.namespace "0.3.0-alpha3"]
                                   [org.clojure/tools.trace "0.7.9"]]
                    :source-paths ["dev"]
