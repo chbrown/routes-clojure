73b703c621efd8b9172c0e9ec124da0183fff229
Christopher Brown <io@henrian.com>
2017-08-29 10:41:12 -0500

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject routes "0.3.2"
+(defproject routes "0.3.3-SNAPSHOT"
   :description "URL-driven routes handling"
   :url "https://github.com/chbrown/routes-clojure"
   :license {:name "Eclipse Public License"
